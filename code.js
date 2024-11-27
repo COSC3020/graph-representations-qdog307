d77ab2638a44819460b2b30b5a6a16a208b2d564
Quincy Morris Reynolds <143751384+qdog307@users.noreply.github.com>
2024-11-26 20:47:52 -0700

    Update

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    if (!Array.isArray(adjMatrix) || adjMatrix.length === 0) {
+        return []; 
+    }
+
+    const adjList = [];
+    for (let i = 0; i < adjMatrix.length; i++) {
+        const neighbors = [];
+        for (let j = 0; j < adjMatrix[i].length; j++) {
+            if (adjMatrix[i][j] === 1) {
+                neighbors.push(j);
+            }
+        }
+        adjList.push(neighbors);
+    }
+    return adjList;
 }
